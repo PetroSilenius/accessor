3023b3ee017b60927091781e84e5020d65e39feb
Kalle Koskinen <hseven1@gmail.com>
2021-03-04 15:10:50 +0200

    muutoksia

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
+import React from 'react';
 import AuditForm from './components/AuditForm';
 import { Grid } from '@material-ui/core';
 
+/*
+
+Esitiedot auditoijalta: 
+mitä apulaitteita käyttää
+Text-to-speech
+Braille lukija
+Käyttääkö hiirtä
+Disability (heikentynyt värinäkö, näkövammaisuus, kuulovammaisuus, kuurous, kognitiiviset ja kielelliset vaikeudet)
+Oma kuvaus
+Auditointiin liittyvä kokemus / tekninen osaaminen / internetin käyttö
+
+
+*/
+
+
+
+
 function App() {
   return (
-    <Grid container spacing={3}>
-      <AuditForm />
-    </Grid>
+    <React.Fragment>
+      <Grid container spacing={3}>
+        <AuditForm />
+      </Grid>
+    </React.Fragment>
   );
 }
 
