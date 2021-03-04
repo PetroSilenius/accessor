a27f252eeb9cb36b42df3adc127cd291cb006da1
Petro Silenius <petro.silenius@gmail.com>
2021-03-04 15:19:52 +0200

    Update firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import 'firebase/firestore';
 import 'firebase/auth';
 import 'firebase/analytics';
 firebase.initializeApp({
-	apiKey: "AIzaSyCjDCDnCi7PFNbgETCkXW7Xne11RaGJWg4",
-	authDomain: "lpbd-team-3.firebaseapp.com",
-	projectId: "lpbd-team-3",
-	storageBucket: "lpbd-team-3.appspot.com",
-	messagingSenderId: "1056081971438",
-	appId: "1:1056081971438:web:1a43afa1145b21118b8bff",
-	measurementId: "G-LKP4G972NK"
-  })
+  apiKey: 'AIzaSyCjDCDnCi7PFNbgETCkXW7Xne11RaGJWg4',
+  authDomain: 'lpbd-team-3.firebaseapp.com',
+  projectId: 'lpbd-team-3',
+  storageBucket: 'lpbd-team-3.appspot.com',
+  messagingSenderId: '1056081971438',
+  appId: '1:1056081971438:web:394c24e82c9fef3b8b8bff',
+  measurementId: 'G-Y1L8RH49WR',
+});
 
-  export default firebase;
+export default firebase;
