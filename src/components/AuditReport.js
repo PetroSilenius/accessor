a41e9b6dd7e2038ab3198b1c88d5bf9df3923c06
Petro Silenius <petro.silenius@gmail.com>
2021-03-08 00:55:33 +0200

    Add questions data to report page

--- src/components/AuditReport.js
+++ src/components/AuditReport.js
@@ -1,16 +1,57 @@
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { firestore } from '../firebase';
-import { useDocumentData } from 'react-firebase-hooks/firestore';
+import { useDocumentData, useCollectionData } from 'react-firebase-hooks/firestore';
+import { Paper, Tabs, Tab } from '@material-ui/core';
+import AuditReportQuestions from './AuditReportQuestions';
 
 function AuditReport() {
   const { auditId } = useParams();
-  console.log(auditId);
-  const auditsRef = firestore.doc(`audits/${auditId}`);
+  const auditRef = firestore.doc(`audits/${auditId}`);
+  const [pageId, setPageId] = useState('Bl97iRBILC0gOYmUgtbc');
+  const questionsRef = firestore.collection('questions');
+  const [questions] = useCollectionData(questionsRef, { idField: 'id' });
 
-  const [audit] = useDocumentData(auditsRef, { idField: 'id' });
+  /*
+  Get pages ids
+
+  useEffect(() => {
+    const getPages = async () => {
+      let pages = [];
+      await firestore
+        .collection('audits')
+        .doc(auditId)
+        .collection('pages')
+        .get()
+        .then((snapshot) => snapshot.forEach((doc) => pages.push(doc.id)));
+      return pages;
+    };
+    setPageId(getPages().json());
+  }, []);
+  */
+
+  const handleChange = (event, newValue) => {
+    setPageId(newValue);
+  };
+
+  const [audit] = useDocumentData(auditRef, { idField: 'id' });
+
   return (
     <>
-      <p>{audit}</p>
+      <Paper>
+        <Tabs
+          value={pageId}
+          onChange={handleChange}
+          indicatorColor="primary"
+          textColor="primary"
+          centered>
+          <Tab label="Item One" />
+          <Tab label="Item Two" />
+          <Tab label="Item Three" />
+        </Tabs>
+      </Paper>
+
+      <AuditReportQuestions auditId={auditId} pageId={pageId} questions={questions} />
     </>
   );
 }
