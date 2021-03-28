672dc5d0c4bc2197b43b494b2028fcb701d32f78
Petro Silenius <petro.silenius@gmail.com>
2021-03-28 17:24:33 +0300

    Add basic user info to users collection and use that in listing, add button to contact auditor

--- src/components/AuditorListing/ProfileCard.js
+++ src/components/AuditorListing/ProfileCard.js
@@ -1,60 +1,49 @@
-import {
-	Container,
-	Grid,
-	makeStyles,
-	Paper,
-	Typography,
-  } from "@material-ui/core";
-  import { Rating } from "@material-ui/lab";
-  
-  const useStyles = makeStyles((theme) => ({
-	container: {
-	  backgroundColor: theme.palette.secondary.main,
-	  backgroundImage: theme.backgroundImage,
-	  backgroundAttachment: "fixed",
-	  backgroundSize: "cover",
-	  minHeight:700
-	},
-	card: {
-	  backgroundColor: theme.palette.secondary.lighter,
-	},
-	profileImg: {
-	  width: 100,
-	  margin: "auto",
-	},
-	submitButton: {
-	  margin: 20,
-	  "& button": {
-		float: "right",
-	  },
-	},
-  }));
-  
+import { Button, Grid, makeStyles, Paper, Typography } from '@material-ui/core';
+import { Rating } from '@material-ui/lab';
+import { useHistory } from 'react-router-dom';
 
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    padding: '10px',
+  },
+  profileImg: {
+    width: 100,
+    margin: 'auto',
+  },
+  submitButton: {
+    margin: 20,
+    '& button': {
+      float: 'right',
+    },
+  },
+}));
 
 export default function ProfileCard({ user }) {
-	const classes = useStyles();
-	return (
-	  <Grid item xs={3}>
-		<Paper style={{padding:10}} >
-			<Grid container direction="column">
-			<img className={classes.profileImg} src={user.photoUrl}></img>
-			<Grid container justify="center">
-			  <div style={{ width: 120 }}>
-				<Rating
-				  name="disabled"
-				  value={user.rating}
-				  precision={0.5}
-				  disabled
-				/>
-			  </div>
-			</Grid>
-			<Typography variant="h6">{user.company}</Typography>
-			<Typography>"{user.description}"</Typography>
-			<Typography style={{fontWeight:"bold"}}>{user.hourlyRate}€/h</Typography>
-			</Grid>
-		</Paper>
-	  </Grid>
-	);
-  }
-  
+  const classes = useStyles();
+
+  const history = useHistory();
+
+  return (
+    <Grid item xs={3}>
+      <Paper className={classes.paper}>
+        <Grid container direction="column">
+          <img className={classes.profileImg} src={user.photoURL} alt="auditor profile" />
+          <Grid container justify="center">
+            <div style={{ width: 120 }}>
+              <Rating name="disabled" value={user.rating} precision={0.5} disabled />
+            </div>
+          </Grid>
+          <Typography variant="h6">{user.company}</Typography>
+          <Typography>"{user.description}"</Typography>
+          <Typography style={{ fontWeight: 'bold' }}>{user.hourlyRate}€/h</Typography>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => history.push(`/posting/${user.id}`)}>
+            Contact auditor
+          </Button>
+        </Grid>
+      </Paper>
+    </Grid>
+  );
+}
