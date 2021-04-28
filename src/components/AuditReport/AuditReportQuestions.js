0640707884e383f478d2446ed3ccab2e06f6de66
Kalle Koskinen <hseven1@gmail.com>
2021-04-28 21:15:43 +0300

    käännökset arviointiraporttiin

--- src/components/AuditReport/AuditReportQuestions.js
+++ src/components/AuditReport/AuditReportQuestions.js
@@ -1,14 +1,14 @@
-import { firestore } from '../../firebase';
-import { useDocumentData } from 'react-firebase-hooks/firestore';
-import i18n from '../../utils/i18n';
-import { Card, CardContent, Grid, Link } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import { useTranslation } from 'react-i18next';
+import { Card, CardContent, Grid, Link } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { useDocumentData } from "react-firebase-hooks/firestore";
+import { useTranslation } from "react-i18next";
+import { firestore } from "../../firebase";
+import i18n from "../../utils/i18n";
 
 const useStyles = makeStyles((theme) => ({
   container: {
-    marginTop: '20px',
-    marginBottom: '20px',
+    marginTop: "20px",
+    marginBottom: "20px",
   },
   card: {
     backgroundColor: theme.palette.secondary.lighter,
@@ -26,8 +26,8 @@ function AuditReportQuestions({ auditId, pageId, questions }) {
       <Grid item xs={10}>
         <Card variant="outlined" className={classes.card}>
           <CardContent>
-            <h2>{t('audit_report.subheader')}</h2>
-            <p>{t('audit_report.description')}</p>
+            <h2>{t("audit_report.subheader")}</h2>
+            <p>{t("audit_report.description")}</p>
           </CardContent>
         </Card>
       </Grid>
@@ -41,8 +41,9 @@ function AuditReportQuestions({ auditId, pageId, questions }) {
                 href={`https://www.w3.org/TR/WCAG21/#${question.wcag}`}
                 target="_blank"
                 rel="noreferrer"
-                color="error">
-                <b>Need help with this?</b>
+                color="error"
+              >
+                <b>{t("audit_report.need_help")}</b>
               </Link>
             </CardContent>
           </Card>
--- src/components/AuditReport/AuditorInformation.js
+++ src/components/AuditReport/AuditorInformation.js
@@ -1,11 +1,11 @@
-import { Card, CardContent, Grid, Link } from '@material-ui/core';
-import { makeStyles } from '@material-ui/styles';
-import { useTranslation } from 'react-i18next';
+import { Card, CardContent, Grid, Link } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
+import { useTranslation } from "react-i18next";
 
 const useStyles = makeStyles((theme) => ({
   container: {
-    marginTop: '20px',
-    marginBottom: '20px',
+    marginTop: "20px",
+    marginBottom: "20px",
   },
   card: {
     backgroundColor: theme.palette.secondary.lighter,
@@ -21,13 +21,13 @@ function AuditorInformation({ user }) {
       <Grid item xs={10}>
         <Card variant="outlined" className={classes.card}>
           <CardContent>
-            <h1>{t('audit_report.header')}</h1>
-            <h2>{t('user_info.header')}</h2>
+            <h1>{t("audit_report.header")}</h1>
+            <h2>{t("user_info.header")}</h2>
             <p>{user?.company}</p>
             <p>{user?.language}</p>
             <p>{user?.description}</p>
             <Link href="#" color="error">
-              <b>Contact auditor</b>
+              <b>{t("audit_report.contact")}</b>
             </Link>
           </CardContent>
         </Card>
